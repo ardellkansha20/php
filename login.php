c440fa5f88eb7ad6a89ec44184cf7b48216703b7
ardellkansha20 <ardellkansha20@gmail.com>
2025-05-14 11:55:41 +0700

    memperbaiki error koneksi

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $stmt = $koneksi->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
     $stmt->bind_param("ss", $username, $password);
     $stmt->execute();
     $result = $stmt->get_result();
